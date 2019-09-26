a543a6d92eb94c981b15d61c245b066771331647
RileyBristow <astoniteriley@gmail.com>
2019-09-26 21:06:48 +1000

    Added tower object and made it placeable with temporary sounds

---
+++ Tower Defense Game/Assets/PlaceTower.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceTower : MonoBehaviour
+{
+    public GameObject towerPrefab;
+    private GameObject tower;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private bool CanPlaceMonster()
+    {
+        return tower == null;
+    }
+    void OnMouseUp()
+    {
+        if (CanPlaceMonster())
+        {
+            tower = (GameObject)
+              Instantiate(towerPrefab, transform.position, Quaternion.identity);
+            AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+            audioSource.PlayOneShot(audioSource.clip);
+
+            //remove gold
+        }
+    }
+}
